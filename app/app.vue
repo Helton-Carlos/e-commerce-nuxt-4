718255035a2762eae0f8b661f3bd0fd445985628
Helton Brito <heltoncarlospa@hotmail.com>
2025-08-02 17:26:23 -0300

    chore: add project e-commerce

---
+++ app/app.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+useSeoMeta({
+  title: 'E-commerce App',
+  description: 'Loja feito com nuxt 4.',
+});
+</script>
+
+<template>
+  <div id="app">
+    <NuxtPage />
+  </div>
+</template>
---
+++ app/pages/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <h1 class="text-center font-bold text-8xl text-red-800">teste</h1>
+
+    <button class="btn btn-neutral">Neutral</button>
+    <button class="btn btn-primary">Primary</button>
+    <button class="btn btn-secondary">Secondary</button>
+    <button class="btn btn-accent">Accent</button>
+    <button class="btn btn-info">Info</button>
+    <button class="btn btn-success">Success</button>
+    <button class="btn btn-warning">Warning</button>
+    <button class="btn btn-error">Error</button>
+  </div>
+</template>
