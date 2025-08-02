ee4edd750c6145158dfc3542ce65148bd203abd4
Helton Brito <heltoncarlospa@hotmail.com>
2025-08-02 17:42:34 -0300

    feat: add banner in main component

---
+++ app/components/Main.vue
@@ -0,0 +1,19 @@
+<template>
+  <div
+    class="hero min-h-screen md:min-h-[490px]"
+    style="
+      background-image: url(https://f.i.uol.com.br/fotografia/2023/02/09/167598321663e579707214b_1675983216_3x2_rt.jpg);
+    "
+  >
+    <div class="hero-overlay"></div>
+    <div class="hero-content text-neutral-content text-center">
+      <div class="max-w-md">
+        <h1 class="mb-5 text-5xl font-bold">E-commerce Store</h1>
+        <p class="mb-5">
+          Loja de e-commerce feita com Nuxt 4, Tailwind CSS e DaisyUI.
+        </p>
+        <button class="btn btn-primary">Veja mais</button>
+      </div>
+    </div>
+  </div>
+</template>
--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,14 +1,5 @@
 <template>
   <div>
-    <h1 class="text-center font-bold text-8xl text-white">teste</h1>
-
-    <button class="btn btn-neutral">Neutral</button>
-    <button class="btn btn-primary">Primary</button>
-    <button class="btn btn-secondary">Secondary</button>
-    <button class="btn btn-accent">Accent</button>
-    <button class="btn btn-info">Info</button>
-    <button class="btn btn-success">Success</button>
-    <button class="btn btn-warning">Warning</button>
-    <button class="btn btn-error">Error</button>
+    <Main />
   </div>
 </template>
