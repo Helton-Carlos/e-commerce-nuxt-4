0f50b32adaef3322b7128c807c0eb2e585cc8caa
Helton Brito <heltoncarlospa@hotmail.com>
2025-08-04 20:27:52 -0300

    feat: add footer

---
+++ app/components/Footer.vue
@@ -0,0 +1,22 @@
+<template>
+  <footer class="w-full bg-black text-white mt-10 pt-10">
+    <div class="container flex justify-between">
+      <div>
+        <h1 class="mb-5 text-2xl font-bold">E-commerce Store</h1>
+        <p class="mb-5">
+          Loja de e-commerce feita com Nuxt 4, Tailwind CSS e DaisyUI.
+        </p>
+      </div>
+
+      <nav>
+        <ul>
+          <li>Sobre-nós</li>
+        </ul>
+      </nav>
+    </div>
+
+    <div class="text-center mt-5 py-2">
+      Todos os direitos reservados &copy; {{ new Date().getFullYear() }}
+    </div>
+  </footer>
+</template>
