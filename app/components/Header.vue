0a940ff42134a74ef4ee9a045f7ecc80308c6dbe
Helton Brito <heltoncarlospa@hotmail.com>
2025-09-14 20:20:46 -0300

    feat: add component menu

--- app/components/Header.vue
+++ app/components/Header.vue
@@ -3,17 +3,7 @@ import { useUserStore } from '@/stores/user';
 import type { CartItem } from '@/types/cart.types';
 import { useSafeLocalStorage } from '~/composable/localStorage';
 
-const userStore = useUserStore();
-
 const carts = useSafeLocalStorage<CartItem[]>('cart', []);
-
-onMounted(() => {
-  userStore.setUser({
-    id: 1,
-    name: 'Maria da Silva',
-    email: 'maria@example.com',
-  });
-});
 </script>
 
 <template>
@@ -29,35 +19,7 @@ onMounted(() => {
             <Cart :carts="carts" />
           </div>
 
-          <div class="dropdown dropdown-end">
-            <div
-              tabindex="0"
-              role="button"
-              class="btn btn-ghost btn-circle avatar"
-            >
-              <div class="w-10 rounded-full">
-                <img
-                  alt="Tailwind CSS Navbar component"
-                  src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp"
-                />
-              </div>
-            </div>
-            <ul
-              tabindex="0"
-              class="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
-            >
-              <li class="text-2xl">
-                <a class="justify-between">
-                  Profile: {{ userStore.user?.name }}
-                </a>
-              </li>
-              <li class="text-xl">
-                <NuxtLink to="/login">Login</NuxtLink>
-              </li>
-              <li class="text-xl"><a>Settings</a></li>
-              <li class="text-xl"><a>Logout</a></li>
-            </ul>
-          </div>
+          <Menu />
         </div>
       </div>
     </nav>
---
+++ app/components/Menu.vue
@@ -0,0 +1,29 @@
+<script lang="ts" setup>
+const { isAuthenticated } = useUserStore();
+</script>
+
+<template>
+  <div class="dropdown dropdown-end">
+    <div tabindex="0" role="button" class="btn btn-ghost btn-circle avatar">
+      <div class="w-10 rounded-full">
+        <img
+          alt="Tailwind CSS Navbar component"
+          src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp"
+        />
+      </div>
+    </div>
+    <ul
+      tabindex="0"
+      class="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
+    >
+      <li class="text-xl">
+        <NuxtLink to="/login">Login</NuxtLink>
+      </li>
+      <div v-if="isAuthenticated">
+        <li class="text-xl"><a>Perfil</a></li>
+        <li class="text-xl"><a>Configuração</a></li>
+        <li class="text-xl"><a>Sair</a></li>
+      </div>
+    </ul>
+  </div>
+</template>
